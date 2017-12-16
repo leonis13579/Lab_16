2fe2c6cc8016d01f1a351c49e73af4a8781e1bfb
leonis13579 <leonis13579@gmail.com>
2017-12-16 19:44:07 +0300

    Добавьте файлы проекта.

---
+++ Lab16.cpp
@@ -0,0 +1,15 @@
+// Lab16.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cout << a << endl;
+    return 0;
+}
+
